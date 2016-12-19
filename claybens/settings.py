3fb890664a018e6d8b868b83474487d6d2346b7b
Zachary A Bensley <zabensley@gmail.com>
2016-12-18 17:42:13 -0800

    Home: Created home page. Base project setup.

--- claybens/settings.py
+++ claybens/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    # Project App
+    # Project Apps
     'portfolio',
 ]
 
@@ -120,4 +120,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
